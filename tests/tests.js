7c034c6c63258d0870a1f588054956a5548ae428
Joel Lonbeck <joel@neutrinographics.com>
2016-03-15 14:42:57 -0700

    fixed bugs in tests

--- tests/tests.js
+++ tests/tests.js
@@ -33,36 +33,36 @@
 
     it('should create a user', function(done) {
       api.createUser(demoUser, adminUser, false).then(function(user) {
-        assert(user.username, demoUser.username);
+        assert.equal(user.username, demoUser.username);
       }).then(done, done);
     });
 
     it('should search for users', function(done) {
       api.searchUsers(demoUser.username, 5, null).then(function(list) {
-        assert(list.length > 0, true);
+        assert(list.length > 0);
       }).then(done, done);
     });
 
     it('should retrieve the user without an email', function(done) {
       api.getUser(demoUser, null).then(function(user) {
-        assert(user !== null, true);
-        assert(_.isEmpty(user.email), true);
+        assert(user !== null);
+        assert(_.isEmpty(user.email));
       }).then(done, done);
     });
 
     it('should retrieve the user with an email', function(done) {
       api.getUser(demoUser, adminUser).then(function(user) {
-        assert(user !== null, true);
-        assert(!_.isEmpty(user.email), true);
+        assert(user !== null);
+        assert(!_.isEmpty(user.email));
       }).then(done, done);
     });
 
     it('should not retrieve unknown user', function(done) {
       api.getUser(fakeUser, null).then(function(user) {
-        assert(user === null, true);
+        assert(user === null);
       }).then(done, function(response) {
         // error handler
-        assert(true, true);
+        assert(true);
         done();
       });
     });
@@ -70,52 +70,52 @@
     it('should search respositories', function(done) {
       let limit = 5;
       api.searchRepos('uw', 0, limit).then(function(list) {
-        assert(list.length > 0, true);
+        assert(list.length > 0);
         assert(list.length <= limit);
       }).then(done, done);
     });
 
     it('should create a repository', function(done) {
       api.createRepo(demoRepo, demoUser).then(function(repo) {
-        assert(repo !== null, true);
-        assert(repo.full_name, demoUser.username + '/' + demoRepo.name);
+        assert(repo !== null);
+        assert.equal(repo.full_name, demoUser.username + '/' + demoRepo.name);
       }).then(done, done);
     });
 
     it('should list a users repositories', function(done) {
       api.listRepos(demoUser).then(function(list) {
-        assert(list.length > 0, true);
+        assert(list.length > 0);
       }).then(done, done);
     });
 
     it('should delete a repository', function(done) {
       api.deleteRepo(demoRepo, demoUser).then(function() {
-        assert(true, true);
+        assert(true);
       }).then(done, done);
     });
 
     it('should not delete a repository from unknown user', function(done) {
       api.deleteRepo(demoRepo, fakeUser).then(function() {
-        assert(false, true);
+        assert(false);
       }).then(done, function(result) {
-        assert(true, true);
+        assert(true);
         done();
       });
     });
 
     it('should not delete unknown repository', function(done) {
       api.deleteRepo(fakeRepo, demoUser).then(function() {
-        assert(false, true);
+        assert(false);
       }).then(done, function(result) {
-        assert(true, true);
+        assert(true);
         done();
       });
     });
 
     it('should create a token', function(done) {
       api.createToken(demoToken, demoUser).then(function(token) {
-        assert(token != null, true);
-        assert(token.name, demoToken.name);
+        assert(token != null);
+        assert.equal(token.name, demoToken.name);
       }).then(done, done);
     });
 
@@ -130,14 +130,14 @@
         return api.getUser(demoUser, {username: demoUser.username, token: list[0].sha1})
       }).then(function(user) {
         assert(user != null);
-        assert(user.email, demoUser.email);
+        assert.equal(user.email, demoUser.email);
       }).then(done, done);
     });
 
     it('should create a public key', function(done) {
       api.createPublicKey(demoKey, demoUser).then(function(key) {
         assert(key != null);
-        assert(key.title, demoKey.title);
+        assert.equal(key.title, demoKey.title);
       }).then(done, done);
     });
 
@@ -168,21 +168,21 @@
 
     it('should delete a user', function(done) {
       api.deleteUser(demoUser, adminUser).then(function() {
-        assert(true, true);
+        assert(true);
       }).then(done, done);
     });
 
     it('should not error when deleting unknown user', function(done) {
       api.deleteUser(fakeUser, adminUser).then(function() {
-        assert(true, true);
+        assert(true);
       }).then(done, done);
     });
 
     it('should not allow user to delete self', function(done) {
       api.deleteUser(adminUser, adminUser).then(function() {
-        assert(false, true);
+        assert(false);
       }).then(done, function(result) {
-        assert(true, true);
+        assert(true);
         done();
       });
     });
